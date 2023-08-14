d119450f66ee9528964fb1dd2b0fe274a98bfb4f
Ran Kirlian <rankirlian@gmail.com>
2023-08-14 12:40:37 +0200

    update index to add border to canvas item

--- state/ballState.js
+++ state/ballState.js
@@ -6,6 +6,8 @@ class Ball {
     this.ballSize = ballSize;
     this.positionX = 0;
     this.positionY = 0;
+
+    this.state = new topState();
   }
 
   setState(state) {
@@ -17,7 +19,24 @@ class Ball {
   }
 }
 
+class topState {
+  print(ball) {
+    ball.ctx.clearRect(0, 0, ball.width, ball.height);
+
+    ball.ctx.fillRect(
+      ball.positionX,
+      ball.positionY,
+      ball.ballSize,
+      ball.ballSize
+    );
+
+    if (ball.positionX < ball.width - ball.ballSize)
+      ball.positionX += ball.ballSize;
+  }
+}
+
 const ctx = canvas.getContext('2d');
 ctx.fillStyle = 'black';
 
 const ball = new Ball(ctx, canvas, 10);
+setInterval(() => ball.print(), 100);
