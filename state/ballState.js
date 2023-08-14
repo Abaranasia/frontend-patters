efebb895456399fbf46ccff79d0a3a8a962b8ac5
Ran Kirlian <rankirlian@gmail.com>
2023-08-14 13:15:31 +0200

    update ballState to add the additional states

--- state/ballState.js
+++ state/ballState.js
@@ -32,11 +32,59 @@ class topState {
 
     if (ball.positionX < ball.width - ball.ballSize)
       ball.positionX += ball.ballSize;
+    else ball.setState(new rightState());
+  }
+}
+class rightState {
+  print(ball) {
+    ball.ctx.clearRect(0, 0, ball.width, ball.height);
+
+    ball.ctx.fillRect(
+      ball.positionX,
+      ball.positionY,
+      ball.ballSize,
+      ball.ballSize
+    );
+
+    if (ball.positionY < ball.height - ball.ballSize)
+      ball.positionY += ball.ballSize;
+    else ball.setState(new bottomState());
   }
 }
 
+class bottomState {
+  print(ball) {
+    ball.ctx.clearRect(0, 0, ball.width, ball.height);
+
+    ball.ctx.fillRect(
+      ball.positionX,
+      ball.positionY,
+      ball.ballSize,
+      ball.ballSize
+    );
+
+    if (ball.positionX > 0) ball.positionX -= ball.ballSize;
+    else ball.setState(new leftState());
+  }
+}
+
+class leftState {
+  print(ball) {
+    ball.ctx.clearRect(0, 0, ball.width, ball.height);
+
+    ball.ctx.fillRect(
+      ball.positionX,
+      ball.positionY,
+      ball.ballSize,
+      ball.ballSize
+    );
+
+    if (ball.positionY > 0) ball.positionY -= ball.ballSize;
+    else ball.setState(new topState());
+  }
+}
 const ctx = canvas.getContext('2d');
 ctx.fillStyle = 'black';
 
 const ball = new Ball(ctx, canvas, 10);
-setInterval(() => ball.print(), 100);
+setInterval(() => ball.print(), 10);
