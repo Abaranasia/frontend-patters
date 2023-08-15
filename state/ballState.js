1be5bbb7e1aabe74674667fc4d3208d497587259
Ran Kirlian <rankirlian@gmail.com>
2023-08-15 10:03:00 +0200

    Update ballState to include comments and improve the code

--- state/ballState.js
+++ state/ballState.js
@@ -21,6 +21,7 @@ class Ball {
 
 class topState {
   print(ball) {
+    // The next commented line cleans every previous ball print, so it will look like moving around on each iteration
     // ball.ctx.clearRect(0, 0, ball.width, ball.height);
 
     ball.ctx.fillRect(
@@ -82,25 +83,30 @@ class leftState {
     if (ball.positionY > 0) ball.positionY -= ball.ballSize;
     else {
       ball.setState(new topState());
-      desp.setDesplacement(desp.value + 15);
+      desp.setDisplacement(desp.value + 10);
     }
   }
 }
-
-class Desplacement {
+class Displacement {
+  // This class allows to set an incremental value to displace the position of the ball in a required axis on each iteration
   constructor(value) {
     this.value = value;
   }
 
-  setDesplacement(value) {
+  setDisplacement(value) {
     this.value = value;
   }
 }
 
+// ************* Main code *************
+/* This code originally move a ball around the canvas border, but it has been updated 
+to paint a grid by displacing the ball and not cleaning the previous print interaction
+*/
+
 const ctx = canvas.getContext('2d');
 ctx.fillStyle = 'black';
 
-const ball = new Ball(ctx, canvas, 10);
-const desp = new Desplacement(0);
+const ball = new Ball(ctx, canvas, 5); // Creates a ball of 10px size
+const desp = new Displacement(0); // Starts with no displacement, so in position (0,0)
 
-setInterval(() => ball.print(), 10);
+setInterval(() => ball.print(), 10); // Prints a new ball every 10ms
